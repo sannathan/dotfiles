85c3fa23fbf94fb39c797458d1e25ac14c298ed4
Nathan <nathansantos1713@gmail.com>
2025-05-22 12:59:58 -0300

    adding kitty, neofetch and nvim configuration

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,17 @@
+-- Esse código é o responsável por todos os formatadores
+
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
